5e611abce982dc1f2c356c262d30ab220379b1dd
Markella Chorianopoulou <markellachor1997@gmail.com>
2023-01-25 20:30:06 +0200

    read users endpoint

--- AmsApi/api/urls.py
+++ AmsApi/api/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 
 urlpatterns = [
     path('login', views.LoginView.as_view()),
-    path('me', views.UserView.as_view())
+    path('me', views.MeView.as_view()),
+    path('users/<int:id>', views.UserView.as_view()),
+    path('users', views.UsersView.as_view())
 ]
--- AmsApi/api/views/user.py
+++ AmsApi/api/views/user.py
@@ -1,10 +1,43 @@
-from rest_framework import generics
+from django.contrib.auth.models import User
+from django.http import JsonResponse
+from rest_framework import generics, permissions, views
 
 from . import serializers
 
 
-class UserView(generics.RetrieveAPIView):
+class MeView(generics.RetrieveAPIView):
+    """
+    Class representing "me" where me is a logged-in user
+    """
     serializer_class = serializers.UserSerializer
 
     def get_object(self):
         return self.request.user
+
+class UserView(views.APIView):
+    permission_classes = ([permissions.IsAdminUser])
+
+    def get(self, _request, id):
+        try:
+            user_instance = User.objects.get(id=id)
+            data = serializers.UserSerializer(instance=user_instance).data
+            response = JsonResponse(data=data)
+        except User.DoesNotExist:
+            response = JsonResponse({
+                'status_code': 404,
+                'error': 'The resource was not found'
+            }, status=404)
+
+        return response
+
+class UsersView(views.APIView):
+    permission_classes = ([permissions.IsAdminUser])
+    def get(self, _request):
+        users_instance = User.objects.all()
+        data = serializers.UserSerializer(instance=users_instance, many=True).data
+        print(data)
+        response = JsonResponse(data=data, safe=False)
+
+        return response
+        
+        
